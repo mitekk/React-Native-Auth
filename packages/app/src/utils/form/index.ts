39fbcac373a46fab124f333137040188db3a1ecf
Inbal <mitekk@gmail.com>
2022-01-22 22:53:51 +0200

    profile icons hook

--- packages/app/src/utils/form/index.ts
+++ packages/app/src/utils/form/index.ts
@@ -1,5 +1,4 @@
 import {allowanceUtil} from './allowance.util';
-import {avatarUtil} from './avatar.util';
 import {colorUtil} from './color.util';
 
-export {allowanceUtil, avatarUtil, colorUtil};
+export {allowanceUtil, colorUtil};
