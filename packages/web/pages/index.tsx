15a8142287fd39a7b3a709e4a244e04a2f64f2af
Inbal <mitekk@gmail.com>
2022-02-06 23:53:55 +0200

    form basic layout

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,43 +1,11 @@
-import * as React from "react";
 import type { NextPage } from "next";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Link from "../src/Link";
 import Layout from "../src/layout";
+import ResetPasswordForm from "../src/components/resetPasswordForm";
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <Box
-        sx={{
-          my: 4,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          flex: 2,
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-          MUI v5 + Next.js with TypeScript example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-      </Box>
-      <Box
-        sx={{
-          my: 4,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          flex: 3,
-          backgroundColor: "red",
-        }}
-      >
-        as
-      </Box>
+      <ResetPasswordForm></ResetPasswordForm>
     </Layout>
   );
 };
--- packages/web/src/components/resetPasswordForm.tsx
+++ packages/web/src/components/resetPasswordForm.tsx
@@ -1,6 +1,128 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Box } from "@mui/system";
-import React from "react";
+import { Button, IconButton, InputAdornment, TextField } from "@mui/material";
+import { css } from "@emotion/react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+type ResetPasswordFields = {
+  password: string;
+  passwordVerify: string;
+};
 
 export default function () {
-  return <Box></Box>;
+  const [formState, setFormstate] = useState({
+    showPassword: false,
+    showComfirmPassword: false,
+  });
+  const { handleSubmit } = useForm<ResetPasswordFields>();
+
+  const onSubmit = handleSubmit(({ password, passwordVerify }) => {
+    console.log(password, passwordVerify);
+  }); //
+
+  const handleClickShowPassword = () => {
+    setFormstate({
+      ...formState,
+      showPassword: !formState.showPassword,
+    });
+  };
+  const handleClickShowComfirmPassword = () => {
+    setFormstate({
+      ...formState,
+      showComfirmPassword: !formState.showComfirmPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  const styles = {
+    fields: css`
+      width: 50%;
+    `,
+  };
+
+  return (
+    <Box
+      css={css`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        margin-top: 50px;
+      `}
+      autoCapitalize="false"
+    >
+      <TextField
+        id="standard-basic"
+        label="email"
+        variant="standard"
+        margin="normal"
+        css={styles.fields}
+      />
+      <TextField
+        id="standard-basic"
+        name="asda"
+        label="new password"
+        variant="standard"
+        margin="normal"
+        type={formState.showPassword ? "text" : "password"}
+        autoComplete="current-password"
+        css={styles.fields}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {formState.showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+      <TextField
+        id="standard-basic"
+        label="comfirm new password"
+        variant="standard"
+        margin="normal"
+        type={formState.showComfirmPassword ? "text" : "password"}
+        autoComplete="current-password"
+        css={styles.fields}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowComfirmPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {formState.showComfirmPassword ? (
+                  <VisibilityOff />
+                ) : (
+                  <Visibility />
+                )}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+
+      <Button
+        variant="contained"
+        css={css`
+          width: 25%;
+          margin: 50px;
+        `}
+      >
+        Contained
+      </Button>
+    </Box>
+  );
 }
