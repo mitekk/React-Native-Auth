5d463c475c5b6696b78db93fde82a504a0c15d43
Inbal <mitekk@gmail.com>
2022-04-04 23:51:13 +0300

    reset password email styling

--- packages/server/src/handlers/email.handler.ts
+++ packages/server/src/handlers/email.handler.ts
@@ -19,13 +19,89 @@ export const Email = () => {
       from,
       to,
       subject: "Pocket password restore", // Subject line
-      html: `<div>
-      <h1>Password restore for pocket account</h1>
-      <span>Please follow the link to reset your password</span>
-      <span>
-      <a href="http://localhost:3000/?token=${token}" target="_blank">Reset password</a>
-      </span>
-      </div>`, // html body
+      html: `<!DOCTYPE html>
+      <html>
+        <head>
+          <link rel="preconnect" href="https://fonts.googleapis.com">
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+          <style>
+            body {
+              background-color: #FFFCFC;
+                      display: flex;
+              flex-direction: column;
+              align-items: center;
+            }
+      
+            .container {
+              font-family: 'Roboto Slab', serif;
+              background-color: #A7D5B7;
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              width:500px;
+              
+              padding: 25px 25px;
+              border-radius:2px;
+            }
+      
+            .logo {
+              padding-bottom: 20px;
+              font-size: 50px;
+              color: #DABE45;
+            }
+      
+            .box-container {
+              display: flex;
+              flex-direction: column;
+              height: 200px;
+              width: 95%;
+              background-color: #5CCABE;
+              align-items: center;
+              margin: 20px;
+              padding: 30px 0px;
+              border-radius:2px;
+            }
+      
+            .title {
+              padding: 5px;
+              font-size: 25px;
+            }
+      
+            .sub-title {
+              padding: 25px 50px;
+              font-size: 16px;
+            }
+      
+            .button {
+              border: none;
+              color: white;
+              padding: 15px 32px;
+              text-align: center;
+              text-decoration: none;
+              display: inline-block;
+              border-radius:5px;
+              font-size: 15px;
+              margin: 4px 2px;
+              cursor: pointer;
+              background-color: #DABE45;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <span class="logo">Pocket</span>
+            <div class="box-container">
+              <span class="title">Forgot your password?</span>
+              <div class="sub-title">
+                <div>That's ok, it happends!</div>
+                <div>Click on the button below to reset your password</div>
+              </div>
+              <button autofocus class="button" onclick="location.href='http://localhost:3000/?token=${token}'"> Reset password </button>
+            </div>
+          </div>
+        </body>
+      </html>`, // html body
     });
 
   const sendVerifyEmail = ({ to }: { to: string; token: string }) =>
