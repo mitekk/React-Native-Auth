7e0ba336e5fdd71bdc0ac474ceed9912339713e5
Inbal <mitekk@gmail.com>
2022-04-05 00:08:57 +0300

    verify email styling minify

--- packages/server/src/handlers/email.handler.ts
+++ packages/server/src/handlers/email.handler.ts
@@ -18,19 +18,24 @@ export const Email = () => {
     transporter.sendMail({
       from,
       to,
-      subject: "Pocket password restore", // Subject line
+      subject: "Pocket password reset", // Subject line
       html: `<!DOCTYPE html><html><head><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet"><style>body{background-color:#fffcfc;display:flex;flex-direction:column;align-items:center}.container{font-family:'Roboto Slab',serif;background-color:#a7d5b7;display:flex;flex-direction:column;align-items:center;width:500px;padding:25px 25px;border-radius:2px}.logo{padding-bottom:20px;font-size:50px;color:#dabe45}.box-container{display:flex;flex-direction:column;height:200px;width:95%;background-color:#5ccabe;align-items:center;margin:20px;padding:30px 0;border-radius:2px}.title{padding:5px;font-size:25px}.sub-title{padding:25px 50px;font-size:16px}.button{border:none;color:#fff;padding:15px 32px;text-align:center;text-decoration:none;display:inline-block;border-radius:5px;font-size:15px;margin:4px 2px;cursor:pointer;background-color:#dabe45}</style></head><body><div class="container"><span class="logo">Pocket</span><div class="box-container"><span class="title">Forgot your password?</span><div class="sub-title"><div>That's ok, it happends!</div><div>Click on the button below to reset your password</div></div><button autofocus class="button" onclick='location.href="http://localhost:3000/?token=${token}"'>Reset password</button></div></div></body></html>`, // html body
     });
 
-  const sendVerifyEmail = ({ to }: { to: string; token: string }) =>
+  const sendVerifyEmail = ({
+    to,
+    name,
+    token,
+  }: {
+    to: string;
+    name: string;
+    token: string;
+  }) =>
     transporter.sendMail({
       from: '"Pocket 💰" <support@pocket.com>',
       to,
       subject: "Pocket email verification",
-      html: `<div>
-      <span>Please follow the link to verify your email</span>
-      <span></span>
-      </div>`,
+      html: `<!DOCTYPE html><html><head><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet"><style>body{background-color:#fffcfc;display:flex;flex-direction:column;align-items:center}.container{font-family:'Roboto Slab',serif;background-color:#a7d5b7;display:flex;flex-direction:column;align-items:center;width:500px;padding:25px 25px;border-radius:2px}.logo{padding-bottom:20px;font-size:50px;color:#dabe45}.box-container{display:flex;flex-direction:column;height:270px;width:95%;background-color:#5ccabe;align-items:center;margin:20px;padding:30px 0;border-radius:2px}.title{padding:5px;font-size:25px}.sub-title{padding:25px 50px;font-size:16px}.button{border:none;color:#fff;padding:15px 32px;text-align:center;text-decoration:none;display:inline-block;border-radius:5px;font-size:15px;margin:4px 2px;cursor:pointer;background-color:#dabe45}</style></head><body><div class="container"><span class="logo">Pocket</span><div class="box-container"><span class="title">Verify your e-mail address</span><div class="sub-title"><div style="margin:10px 0">Hey ${name}!</div><div style="margin:10px 0">Thank you for choosing Pocket application.</div><span>Please confirm that</span><b>${to}</b><span>is your e-mail address by clicking the button bellow.</span></div><button autofocus class="button" onclick='location.href="http://localhost:3000/verify/?token=${token}"'>Verify</button></div></div></body></html>`,
     });
 
   return { sendPasswordRestore, sendVerifyEmail };
