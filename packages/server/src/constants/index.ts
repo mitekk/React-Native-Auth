0e8c9cd715495dc47b260a178c5bb6ce6818a1c2
Inbal <mitekk@gmail.com>
2022-04-09 19:16:46 +0300

    add constant environment vars

--- packages/server/src/constants/index.ts
+++ packages/server/src/constants/index.ts
@@ -4,3 +4,7 @@ dotenv.config();
 export const __prod__ = process.env.NODE_ENV === "production";
 export const JWT_SECRET_ACCESS_TOKEN = process.env.JWT_SECRET_ACCESS_TOKEN!;
 export const JWT_SECRET_REFRESH_TOKEN = process.env.JWT_SECRET_REFRESH_TOKEN!;
+export const DB_NAME = process.env.DATABASE_NAME;
+export const DB_TYPE = process.env.DATABASE_TYPE;
+export const DB_USER = process.env.DATABASE_USER;
+export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
